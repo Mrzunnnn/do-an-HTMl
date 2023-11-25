c45990226d9a74999823f7cd7616d1d0fdce8f7d
Mrzunnnn <148939431+Mrzunnnn@users.noreply.github.com>
2023-11-25 11:48:48 +0700

    update do an

--- assets/js/custom-owl.js
+++ assets/js/custom-owl.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            },
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      loop: true /*vong lap slider */,
+      margin: 10 /*khoang cach giua 2 PT*/,
+      nav: true /* thanh dieu huong*/,
+      dots: true /*dau cham  */,
+      autoplay: true /* de slider tu chay */,
+      autoplayTimeout: 5000 /*cai dat thoi gian tu chay cua sli */,
+      responsive: {
+        /*Hien thi item tren cac khoang man hinh  */
+        0: {
+          items: 4,
         },
+        600: {
+          items: 4,
+        },
+        1000: {
+          items: 4,
+        },
+      },
     });
   });
